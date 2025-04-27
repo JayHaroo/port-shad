72806a9c8e5e3f7c6bb7c22ac20687bf9e4f0d4d
M-Jey <mjeyict3@gmail.com>
2025-04-27 11:02:57 +0800

    Add Avatar component and integrate it into Main page

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -9,12 +9,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Main() {
   return (
-    <div className="flex flex-col align-center justify-center h-screen">
+    <div className="flex flex-col align-center justify-center">
       <div className="flex flex-col items-center max-h-screen">
         <Label className="text-4xl font-akira p-5">Greetings!</Label>
+        <Avatar className="w-24 h-24">
+          <AvatarImage src="https://avatars.githubusercontent.com/u/97579224?v=4" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
         <div className="">My name is M-Jey L. Saturno</div>
         <div className="">Software, Mobile Developer & Graphic Designer</div>
         <div className="flex align-center justify-center mt-5 justify-evenly w-3/12">
@@ -22,9 +27,11 @@ export default function Main() {
           <Button variant="outline">My Resume</Button>
         </div>
       </div>
+
       <div className="p-5">
         <Separator />
       </div>
+
       <div className="flex flex-col items-center max-h-screen">
         <Label className="text-2xl font-special p-5">Experience</Label>
         <div className="flex justify-around w-11/12">
@@ -35,7 +42,7 @@ export default function Main() {
             </CardHeader>
             <CardContent>
               <p>
-                Worked on a Logo Design, Poster Design, and other Graphic Design    
+                Worked on a Logo Design, Poster Design, and other Graphic Design
               </p>
             </CardContent>
             <CardFooter className="flex justify-between">
@@ -43,7 +50,7 @@ export default function Main() {
               <div>Remote</div>
             </CardFooter>
           </Card>
-          
+
           <Card className="w-96 ">
             <CardHeader>
               <CardTitle>Part-Time Layout & Graphic Designer</CardTitle>
@@ -51,13 +58,13 @@ export default function Main() {
             </CardHeader>
             <CardContent>
               <p>
-                Worked on a various Web2 + Web3 Projects. Assigned to the Mobile
-                Development
+                Worked on a managing the layout of the DTF films and other
+                graphic related works.
               </p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <div>Jan - Mar 2025</div>
-              <div>Remote</div>
+              <div>2022-2023</div>
+              <div>On Site</div>
             </CardFooter>
           </Card>
 
@@ -78,6 +85,12 @@ export default function Main() {
             </CardFooter>
           </Card>
         </div>
+      </div>
+      <div className="p-5">
+        <Separator />
+      </div>
+      <div className="flex flex-col items-center max-h-screen">
+        <Label className="text-2xl font-special p-5">Selected Works</Label>
       </div>
     </div>
   );
