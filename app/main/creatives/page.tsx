9eb1128575c9a64bdc871caad41a2a79042b8ab0
JayHaroo <saturnomjey09@gmail.com>
2025-07-03 06:10:09 +0800

    Add separator to creatives page for improved layout

--- app/main/creatives/page.tsx
+++ app/main/creatives/page.tsx
@@ -15,6 +15,7 @@ export default function creativesPages() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Label className="text-4xl font-akira p-5 max-sm:text-[20px]">My graphic design works</Label>
+      <Separator className="w-1/2 mb-5" />
     </div>
   );
 }   
