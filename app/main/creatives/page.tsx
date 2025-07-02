bd571f550f407cd0663b71d3d4c0c808b3804433
JayHaroo <saturnomjey09@gmail.com>
2025-07-03 06:09:15 +0800

    Refactor creatives page: enhance layout and update heading to showcase graphic design works

--- app/main/creatives/page.tsx
+++ app/main/creatives/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function creativesPages() {
   return (
-    <div>
-      <h1>Creatives Pages</h1>
-      <p>This is the creatives pages section.</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Label className="text-4xl font-akira p-5 max-sm:text-[20px]">My graphic design works</Label>
     </div>
   );
 }   
